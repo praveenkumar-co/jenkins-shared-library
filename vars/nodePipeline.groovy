07e9633a00b650b9874f452be6f5589f4bfe0be5
PRAVEEN KUMAR <praveen.iyu123@gmail.com>
2026-02-26 15:30:25 +0530

    Update Jenkins pipeline echo messages

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -1,3 +1,5 @@
-def call(){
-  echo "Hello World , I am in Jenkins"
+def call() {
+    echo "Node Pipeline started by: ${env.BUILD_USER ?: 'Jenkins'}"
+    echo "Branch: ${env.GIT_BRANCH ?: 'N/A'}"
+    echo "Build #${env.BUILD_NUMBER}"
 }
